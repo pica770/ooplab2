b9b4856c3c521f15869309ad1c190147be7d7f66
hedo <87273975+pica770@users.noreply.github.com>
2023-01-25 19:49:01 +0300

    Update Bernulli_oop.cpp

--- Bernulli_oop.cpp
+++ Bernulli_oop.cpp
@@ -10,21 +10,19 @@ int main()
 	double r;
 	cin >> r;
 
-	cout << "Введите угол: ";
-	int fi;
-	cin >> fi;
-
-	curve cur(r, fi);
+	curve cur(r);
 	int action;
 
 	do
 	{
 		cout << "0.Ввести координаты" << endl;
 		cout << "1.Изменить расстояние между фокусами" << endl;
-		cout << "2.Изменить угол" << endl;
-		cout << "3.Получить информацию и параметрах кривой" << endl;
-		cout << "4.Получить расстояние до центра" << endl;
-		cout << "5.Получить радиус кривизны" << endl;
+		cout << "2.Получить информацию о параметрах кривой" << endl;
+		cout << "3.Получить расстояние до центра" << endl;
+		cout << "4.Получить радиус кривизны от угла" << endl;
+		cout << "5.Получить радиус кривизны от длины радиуса" << endl;
+		cout << "6.Получить площадь сектора" << endl;
+		cout << "7.Получить площадь" << endl;
 
 		cin >> action;
 
@@ -39,30 +37,56 @@ int main()
 		}
 		case 2:
 		{
-			cout << "Введите угол: ";
+			int fi;
+			cout << "Введите угол:" << "\n";
 			cin >> fi;
-			cur.set_fi(fi);
-			break;
-		}
-		case 3:
-		{
 			cout << "Информация о параметрах кривой: ";
 			cout << "\nУгол: " << fi << "\nРасстояние между фокусами: " << r << "\n";
 			break;
 
 		}
-		case 4:
+		case 3:
 		{
-			double dis = cur.get_r(fi, r);
+			int fi;
+			cout << "Введите угол:" << "\n";
+			cin >> fi;
+			double dis = cur.calc_distance(fi);
 			cout << "Расстояние до центра: " << dis << "\n";
 			break;
 		}
-		case 5:
+		case 4:
 		{
-			int rad = cur.get_rad(fi, r);
+			int fi;
+			cout << "Введите угол:" << "\n";
+			cin >> fi;
+			int rad = cur.cur_r_fi(fi);
 			cout << "Радиус кривизны: " << rad << "\n";
 			break;
 
+		}
+		case 5:
+		{
+			int dlina_rad;
+			cout << "Введите длину радиуса" << "\n";
+			cin >> dlina_rad;
+			int rad = cur.cur_r(dlina_rad);
+			cout << "Радиус кривизны: " << rad << "\n";
+			break;
+		}
+		case 6:
+		{
+			int fi;
+			cout << "Введите угол:" << "\n";
+			cin >> fi;
+			double sector = cur.get_sector(fi);
+			cout << "Площадь сектора равна: " << sector << "\n";
+			break;
+		}
+		case 7:
+		{
+			double s = cur.calc_area();
+			cout << "Площадь равна: " << s << "\n";
+			break;
 		}
 		default:
 			break;
