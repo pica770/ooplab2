4cde9affa3aac284847fba98d82862b3a67cc07c
hedo <87273975+pica770@users.noreply.github.com>
2023-01-25 19:49:31 +0300

    Update curve.cpp

--- curve.cpp
+++ curve.cpp
@@ -17,22 +17,22 @@ double curve::get_r()
 
 double curve::calc_distance(int fi)
 {
-	float dis = sqrt(2 * (r / 2) * cos(2 * fi));
+	float dis = sqrt(2 * (r / 2) * (r / 2) * cos(2 * fi));		// расстояние в полярных координатах
 	return dis;
 }
 
 double curve::cur_r_fi(int fi)
 {
-	int dis = sqrt(2 * (r / 2) * (r / 2) * cos(2 * fi));
+	int dis = (2 * (r / 2) * (r / 2)) / (3 * sqrt(2 * (r / 2) * (r / 2) * cos(2 * fi)));    // 2*c^2 / 3р
 	return dis;
 }
 
-/*int curve::cur_r()
+int curve::cur_r(int dlina_rad)
 {
-	int rad = (2 * (r / 2) * (r / 2)) / (3 * );
+	int rad = (2 * (r / 2) * (r / 2)) / (3 * dlina_rad);		// здесь мы указываем длину радиуса и из нее находим радиус кривизны
 	return rad;
 }
-*/
+
 
 double curve::get_sector(int fi)
 {
@@ -42,5 +42,5 @@ double curve::get_sector(int fi)
 
 double curve::calc_area()
 {
-	return r * r;
+	return (r / 2) * (r / 2);		// (r / 2) <=> c
 }
